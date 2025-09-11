bc25e8121ac1e88f8bbd6716455bc16737aae06b
ramyab-986 <ramyab@iisc.ac.in>
2025-09-11 14:20:41 +0530

    Updated files

--- IFN_SequentialDosing.m
+++ IFN_SequentialDosing.m
@@ -51,6 +51,86 @@ for i = 1:length(f1)
     end
 end
 
+%%
+% Parameters
+f1 = [1, 100];   % First dose values
+f2 = [1, 100];   % Second dose values
+lw = 2;          % Line width
+
+% Define markers and colors
+symbols = {'o','s','^','d','v','p','h','x','*','+','.'};
+colors = [0.93, 0.69, 0.13;
+          0.85, 0.33, 0.10];
+
+% Variable names
+var_names = {'ExtVirus','VirusInit','IntVirus','R_{cyt}','(+)RNA_{CM}','SP','NSP','RC_{CM}','dsRNA','RIGI','aRIGI','MAVS','aMAVS',...
+    'IKKe','pIKKe','TBK1','pTBK1','IRF3','pIRF3','IKK','aIKK','NFkBIkBac','pNFkBn','NFkBn','NFkBc','IkBac','IRF7','pIRF7','IFNbmRNA',...
+    'IFNamRNA','IFNlmRNA','IFN_c','IFNl_c','JAK','RJC','STAT1c','CP','ISGn','IFNex','STAT2c','TYK','RTC','ARC','Rec1','Rec2',...
+    'IFNARd','IRF9_c','ARC-STAT2_c','ARC-STAT12_c','STAT2-IRF9_c','ISGF3_c','PSC_c','ISGF3-CP','PSC-CP','NP','STAT1_n','STAT2_n','PIAS','PSC_n',...
+    'IRF9_n','ISGF3_n','PSC-NP','B_u','B_o-NP','B_o','ISGF3-PIAS','STAT2-IRF9_n','ISGF3n-NP','ISGavmRNA','ISGav','ISGnmRNA_n','IRF9mRNA_n',...
+    'IRF7mRNA','ISGnmRNA_c','IRF9mRNA_c'};
+
+% Variables of interest
+v = [69, 74];  %ISGavmRNA, IRF9mRNA_c
+
+% Loop over variables
+for k = 1:length(v)
+    var_idx = v(k);
+
+    % Loop over first dose values
+    for i = 1:length(f1)
+        global_ymax = 0; % reset per plot
+
+        % New figure
+        figure; hold on;
+        title(sprintf('%s, D_1 = %dnM', var_names{var_idx}, f1(i)), 'FontSize', 20);
+
+        % Load first dataset
+        filename1 = sprintf('IFN_24h_%d.mat', f1(i));
+        data1 = load(filename1);
+
+        % Update ymax
+        global_ymax = max(global_ymax, max(data1.Y1(:, var_idx)));
+
+        % Plot first dose
+        plot(data1.T1 / 60, data1.Y1(:, var_idx), ...
+            'LineWidth', lw, ...
+            'DisplayName', sprintf('D_1 = %d only', f1(i)));
+
+        % Loop over second dose values
+        for j = 1:length(f2)
+            filename2 = sprintf('IFN_%d_IFNrest%d_48h.mat', f1(i), f2(j));
+            data2 = load(filename2);
+
+            % Update ymax
+            global_ymax = max(global_ymax, max(data2.Y2(:, var_idx)));
+
+            % Plot second dose
+            plot((data2.T2 / 60) + 24, data2.Y2(:, var_idx), ...
+                'LineWidth', lw, ...
+                'Color', colors(j, :), ...
+                'Marker', symbols{j}, ...
+                'MarkerIndices', 1:10:length(data2.T2), ...
+                'MarkerSize', 8, ...
+                'DisplayName', sprintf('D_1 = %d, D_2 = %d', f1(i), f2(j)));
+        end
+
+        % Mark 24h boundary
+        xline(24, 'k--', 'LineWidth', 2);
+
+        % Formatting
+        set(gca, 'FontSize', 18, 'LineWidth', 1.5, 'TickDir', 'both');
+        xlabel('Time (h)', 'FontSize', 18);
+        ylabel(var_names{var_idx}, 'FontSize', 18);
+        xlim([-2, 74]);
+        ylim([0, global_ymax]);
+
+        % Legend
+        legend('Location', 'northeastoutside', 'AutoUpdate', 'off');
+        legend boxoff;
+    end
+end
+
 %% Chi-sqaured Analysis and Plotting
 
 clear; close all; clc;
